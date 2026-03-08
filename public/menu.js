f3d61828395904f8e2d79da564dfda32d253efcc
Scott <buttersbridge@googlemail.com>
2026-03-08 17:52:36 +0000

    Force redeploy: updated duty.js version tag

--- public/menu.js
+++ public/menu.js
@@ -1,3 +1,22 @@
+const user = requireUser();
+
+document.getElementById("logoutBtn").onclick = () => {
+  localStorage.removeItem("user");
+  window.location.href = "index.html";
+};
+
+document.getElementById("dutyBtn").onclick = () => {
+  window.location.href = "duty.html";
+};
+
+document.getElementById("managerBtn").onclick = () => {
+  if (user.role !== "manager") {
+    alert("Current user doesn't have these permissions");
+    return;
+  }
+  window.location.href = "manager.html";
+};
+
 document.getElementById("changePasswordBtn").onclick = () => {
   window.location.href = "change-password.html";
 };
